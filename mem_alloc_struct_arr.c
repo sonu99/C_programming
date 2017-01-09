7c1fe2398f10bd2765279dcc96cf26f351860359
sonu99 <sonu99@users.noreply.github.com>
2017-01-09 18:54:36 +0530

    Create mem_alloc_struct_arr.c

---
+++ mem_alloc_struct_arr.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Empolyee{
+    char * Name;
+    int    id;
+}EMP;
+
+EMP * *mem_struct(EMP * *);
+void assign_struct(EMP* *);
+void print_struct(EMP* *);
+
+void main()
+{
+    EMP * sonu[5];
+    EMP **sonu_ = NULL;
+    sonu_ = mem_struct(sonu);
+    assign_struct(sonu_);
+    print_struct(sonu_);
+}
+EMP * * mem_struct(EMP* *sonu_){
+    sonu_ = (EMP* *)calloc(sizeof(EMP), 5);
+    return sonu_;
+}
+void assign_struct(EMP* *sonu_){
+    int i;
+    for(i=0;i<5;i++)
+    {
+        sonu_ [i]->Name = "sonu";
+        sonu_ [i]->id = 99;
+    }
+}
+void print_struct(EMP* *sonu_){
+    int i;
+    for(i = 0;i < 5;i++ )
+    {
+        printf("Name:%s\nID:%d\n",sonu_ [i] ->Name,sonu_ [i]->id);
+    }
+}
+
